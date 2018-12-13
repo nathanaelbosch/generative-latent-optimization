eddcf42c185d4f74ed07f67b3378f81fed70698f
Nathanael Bosch <nathanael.bosch@tum.de>
2018-12-13 16:36:54 +0100

    Now using ignite
    Apart from that, I also updated the logger and did some minor refactoring.
    Major change is still the usage of **pytorch-ignite**!
    Code becomes more modular, as logging is done in self-contained files. Overall I
    would say the structure gets better, but it feels very different to read than
    normal pytorch scripts.

--- main.py
+++ main.py
@@ -3,20 +3,37 @@ import os
 import logging
 
 import torch
-from torch.autograd import Variable
 import torch.optim as optim
 import torchvision
 from torchvision import datasets, transforms
 from tensorboardX import SummaryWriter
 import tqdm
 import numpy as np
+from ignite.engine import Events, create_supervised_trainer
+from ignite.metrics import RunningAverage, Loss
 
 from model import Generator
-from utils import IndexToImageDataset, pca_init, LapLoss, project_l2_ball
+from utils import IndexToImageDataset, pca_init, LapLoss
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
+def setup_logger(level='DEBUG'):
+    """Setup personal logger and its handler for this module
+
+    All of this is necessary in order to only get the debug messages from this
+    module, otherwise I get tons of messages from all possible third party
+    imports.
+    """
+    logger = logging.getLogger(__name__)
+    hdlr = logging.StreamHandler()
+    formatter = logging.Formatter(
+        fmt='%(levelname)s|%(name)s|%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    logger.setLevel(level)
+    return logger
+
+
+logger = setup_logger()
 writer = SummaryWriter(comment='')
 
 
@@ -52,58 +69,10 @@ def get_dataloader(dataset, batch_size, data_path='./data'):
     return train_loader
 
 
-def train_epoch(model, device, train_loader, optimizer, loss_fn, epoch,
-                epochs):
-    """Train the model for a single epoch on the train data"""
-    running = defaultdict(int)
-
-    model.train()
-    train_bar = tqdm.tqdm(train_loader)
-    for indices, data in train_bar:
-        data = data.to(device)
-        optimizer.zero_grad()
-        output = model(indices)
-        loss = loss_fn(output, data)
-        loss.backward()
-        optimizer.step()
-
-        batch_size = data.size(0)
-        running['total_seen'] += batch_size
-        running['loss_total'] += loss * batch_size
-        running['loss_avg'] = running['loss_total'] / running['total_seen']
-
-        train_bar.set_description(
-            desc=('[Epoch {:d}/{:d}] Loss: {:.4f}').format(
-                epoch,
-                epochs,
-                running['loss_avg'],
-            ))
-
-    log_loss(running['loss_avg'], epoch)
-    log_parameters(model, epoch)
-
-
-def log_loss(loss, epoch):
-    writer.add_scalar('metrics/train_loss', loss, epoch)
-
-
-def log_parameters(model, epoch):
-    for tag, value in model.named_parameters():
-        tag = tag.replace('.', '/')
-        writer.add_histogram(tag, value.data.cpu().numpy(), epoch)
-        writer.add_histogram(tag + '/grad',
-                             value.grad.data.cpu().numpy(), epoch)
-
-
 def log_images(images, epoch, tag='image'):
     writer.add_image(
         tag, torchvision.utils.make_grid(images, nrow=5, normalize=True),
         epoch)
-
-
-def validate(model, test_indices, epoch):
-    output = model(test_indices)
-    log_images(output, epoch, 'reconstructed_image')
 
 
 def main(
@@ -119,10 +88,15 @@ def main(
         latent_size: ('', 'option') = 100,
         epochs: ('', 'option') = 1000,
         dataset: ('', 'option') = 'cifar10',
+        tensorboard_description: ('', 'option') = '',
+        no_cuda: ('Do not use CUDA, even if available.', 'flag',
+                  'no-cuda') = False,
 ):
-    use_cuda = torch.cuda.is_available()
-    torch.manual_seed(seed)
+    use_cuda = torch.cuda.is_available() and not no_cuda
     device = torch.device('cuda' if use_cuda else 'cpu')
+    if tensorboard_description != '':
+        tensorboard_description = '_' + tensorboard_description
+    torch.manual_seed(seed)
 
     train_loader = get_dataloader(dataset, batch_size, data_path)
 
@@ -133,6 +107,8 @@ def main(
 
     _path = '/tmp/GLO_{}_pca_latent_init.pt'.format(dataset)
     if os.path.isfile(_path):
+        logger.info('PCA already calculated before - Using local file {}'.
+                    format(_path))
         Z = torch.load(_path)
     else:
         Z = pca_init(train_loader, latent_size, device=device)
@@ -148,7 +124,8 @@ def main(
     test_indices = torch.randint(
         len(train_loader.dataset), size=(10, )).to(torch.int64).to(device)
     test_images = [train_loader.dataset[int(i)][1] for i in test_indices]
-    test_images = torch.cat([x.view(1, *x.size()) for x in test_images])
+    test_images = torch.cat(
+        [x.view(1, *x.size()) for x in test_images]).to(device)
     log_images(test_images, 0, 'original_image')
 
     optimizer = optim.Adam([
@@ -165,11 +142,58 @@ def main(
     ])
 
     loss_fn = LapLoss(max_levels=3)
-    validate(model, test_indices, 0)
-    for epoch in range(1, epochs + 1):
-        train_epoch(model, device, train_loader, optimizer, loss_fn, epoch,
-                    epochs)
-        validate(model, test_indices, epoch)
+    trainer = create_supervised_trainer(
+        model, optimizer, loss_fn, device=device)
+
+    desc = '[Epoch {:d}/{:d}] Loss: {:.4f}'
+    log_interval = 1
+    pbar = tqdm.tqdm(total=len(train_loader), desc=desc.format(0, epochs, 0))
+
+    @trainer.on(Events.EPOCH_STARTED)
+    def initialize_running_loss(engine):
+        engine.state.running_loss = 0
+        engine.state._running_loss_sum = 0
+
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def calculate_running_loss(engine):
+        iter = (engine.state.iteration - 1) % len(train_loader) + 1
+        engine.state._running_loss_sum += engine.state.output
+        engine.state.running_loss = engine.state._running_loss_sum / iter
+
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def update_progress_bar(engine):
+        iter = (engine.state.iteration - 1) % len(train_loader) + 1
+
+        if iter % log_interval == 0:
+            pbar.desc = desc.format(engine.state.epoch, epochs,
+                                    engine.state.running_loss)
+            pbar.update(log_interval)
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def refresh_progress_bar(engine):
+        pbar.n = pbar.last_print_n = 0
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_training_loss(engine):
+        epoch = engine.state.epoch
+        writer.add_scalar('metrics/train_loss', engine.state.running_loss,
+                          epoch)
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_model_parameters(engine):
+        epoch = engine.state.epoch
+        for tag, value in model.named_parameters():
+            tag = tag.replace('.', '/')
+            writer.add_histogram(tag, value.data.cpu().numpy(), epoch)
+            writer.add_histogram(tag + '/grad',
+                                 value.grad.data.cpu().numpy(), epoch)
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_reconstructed_images(engine):
+        output = model(test_indices)
+        log_images(output, engine.state.epoch, 'reconstructed_image')
+
+    trainer.run(train_loader, max_epochs=epochs)
 
 
 if __name__ == '__main__':
